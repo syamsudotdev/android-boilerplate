e12fcebdc99728d35fbf79dc5f869d69b1dcb353
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-01-04 10:23:43 +0700

    clean up import, remove test sample

--- app/src/main/java/net/mnsam/applicationboilerplate/MainApplication.kt
+++ app/src/main/java/net/mnsam/applicationboilerplate/MainApplication.kt
@@ -1,7 +1,6 @@
 package net.mnsam.applicationboilerplate
 
 import android.app.Application
-import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
